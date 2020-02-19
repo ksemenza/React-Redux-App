a8d8b786a5d0b7a9e2e87c3c7e1db25a9153b619
unknown <ksemenza13@gmail.com>
2020-02-19 15:49:10 -0500

    created reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,41 @@
+import {
+    FETCH_DATA_START,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAILURE
+    
+} from '../actions'
+
+const initialState = {
+    jokes: null,
+    error: '',
+    isFetching: false
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_DATA_START:
+            return {... state,
+            error: '',
+        isFetching: true
+    };
+    case FETCH_DATA_FAILURE:
+        return{
+            ...state,
+            error: action.payload,
+            isFetching: false
+        };
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                jokes: action.playload,
+                error: '',
+                isFetching: false
+            }
+            default:
+                return state;
+    
+    
+        }
+}
+
+export default reducer
