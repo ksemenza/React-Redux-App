023263054f6fa08432b45d8bef4180b75d4e82b7
ksemenza <ksemenza13@gmail.com>
2020-07-25 15:09:32 -0400

    merge

--- src/assets/styles/styled.js
+++ src/assets/styles/styled.js
@@ -2,15 +2,16 @@ import styled from 'styled-components'
 
 
 export const WelcomeHeader = styled.div `
-border:0.4rem dashed white;
+border:0.4rem dashed #00ff40;
 margin: 1.5rem auto 1.5rem auto;
 padding:2.5rem;
 width:80rem;
-background-color:#00ff40;
+background-color:black;
 color:white;
 justify-content:center;
 h2 {
-    font-size:4.2rem;
+    font-size:7.2rem;
+    font-family:'computer-pixel';
 }
 
 p {
